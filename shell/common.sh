2b4254712be05171d9766751f24b61c992ac525b
Timothée Ravier <tim@siosm.fr>
2020-06-03 12:45:07 +0200

    shell: improve alias handling & include new functions

--- shell/common.sh
+++ shell/common.sh
@@ -3,68 +3,72 @@
 # Set solarized colors for ls
 eval $(dircolors -b ~/.shell/dircolors.256dark)
 
-# Colors related aliases
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-alias diff='colordiff'
-alias ip='ip -color'
-alias bat='bat --theme TwoDark'
-
-# 'ls' related aliases
-alias l='exa'
-alias la='ls --human-readable --almost-all'
-alias ll='exa --long'
-alias lla='ls --color=auto --human-readable -l --all'
-#alias ldir='ls -l| egrep "^d"'
-
-# Misc
-alias bashrc='vim ~/.bashrc'
-alias vimrc='vim ~/.vimrc'
-alias zshrc='vim ~/.zshrc'
-
-# systemd
-alias sdr='sudo systemctl daemon-reload'
-alias ssr='sudo systemctl restart'
-alias sss='sudo systemctl status'
-alias sje='sudo journalctl --pager-end'
-alias sjf='sudo journalctl --follow --lines=100'
-
-# Overload and others
-alias ..='cd ..'
-alias c='cd'
-alias df='df --human-readable'
-alias dmesg="sudo dmesg --human"
-alias du='du --human-readable'
-alias g='git'
-alias gi='git'
-alias gt='git'
-alias gg='git g'
-alias gdb='gdb --quiet'
-alias gr='grep --dereference-recursive --line-number --ignore-case'
-alias less='less --long-prompt --RAW-CONTROL-CHARS'
-alias m='make'
-alias ma='make'
-alias mak='make'
-alias mutt='(cd && mutt)'
-alias pyserv='python -m http.server'
-alias syncmail='mbsync --all ; notmuch new'
-alias tree='tree -pugh'
-alias tm='tmux'
-alias v='vim'
-alias vi='vim'
-alias xo='xdg-open'
-
-
 # Environment variables
 export GREP_COLOR="1;33"
 export EDITOR="/usr/bin/vim"
 export GIT_EDITOR='/usr/bin/vim'
 
 # Longer lines for pdflatex output
-export max_print_line=1000
+# export max_print_line=1000
 
+# Aliases
+alias ..='cd ..'
+alias bashrc='vim ~/.bashrc'
+alias bat='bat --theme TwoDark'
+alias c='cd'
+alias df='df --human-readable'
+alias dmesg="sudo dmesg --human"
+alias du='du --human-readable'
+alias g='git'
+alias gdb='gdb --quiet'
+alias gg='git g'
+alias gi='git'
+alias gr='grep --dereference-recursive --line-number --ignore-case'
+alias grep='grep --color=auto'
+alias gt='git'
+alias ip='ip -color'
+alias la='ls --human-readable --almost-all'
+alias less='less --long-prompt --RAW-CONTROL-CHARS'
+alias lla='ls --color=auto --human-readable -l --all'
+alias ls='ls --color=auto'
+alias m='make'
+alias ma='make'
+alias mak='make'
+alias sdr='sudo systemctl daemon-reload'
+alias sje='sudo journalctl --pager-end'
+alias sjf='sudo journalctl --follow --lines=100'
+alias ssr='sudo systemctl restart'
+alias sss='sudo systemctl status'
+alias tm='tmux'
+alias tree='tree -pugha'
+alias v='vim'
+alias vi='vim'
+alias vimrc='vim ~/.vimrc'
+alias xo='xdg-open'
+alias zshrc='vim ~/.zshrc'
 
-# Various functions
+# "Function" aliases that can not be expressed as regular aliases
+diff() {
+    if [[ -z "$(command -v colordiff)" ]]; then
+        diff "${@}"
+    else
+        colordiff "${@}"
+    fi
+}
+function l() {
+    if [[ -z "$(command -v exa)" ]]; then
+        ls --color=auto "${@}"
+    else
+        exa "${@}"
+    fi
+}
+function ll() {
+    if [[ -z "$(command -v exa)" ]]; then
+        ls --color=auto --human-readable -l "${@}"
+    else
+        exa --long "${@}"
+    fi
+}
 
 # Colored man
 man() {
@@ -81,7 +85,7 @@ man() {
 
 # Create a backup file (.bak)
 backup() {
-    if [ ${#} -ne 1 ]; then
+    if [[ ${#} -ne 1 ]]; then
         printf "Usage : %s <filename>\n" "${0}"
         return
     fi
@@ -90,9 +94,57 @@ backup() {
 
 # Restore a backup file (.bak)
 restore() {
-    if [ ${#} -ne 1 ]; then
+    if [[ ${#} -ne 1 ]]; then
         printf "Usage : %s <filename>\n" "${0}"
         return
     fi
     cp -- "${1}"{.bak,}
+}
+
+sshl() {
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${@}"
+}
+
+shac() {
+    sha256sum -c --ignore-missing "${@}"
+}
+
+run_iso() {
+    local -r iso="${1}"
+    qemu-system-x86_64 -accel kvm -m 4096M -smp cores=2 \
+        -net nic,model=virtio -net user,hostfwd=tcp::2222-:22 \
+        -boot d -cdrom "${iso}"
+}
+
+run_isoc() {
+    local -r iso="${1}"
+    echo "[+] Please append 'console=ttyS0' to GRUB boot entry!"
+    read
+    qemu-system-x86_64 -accel kvm -m 4096M -smp cores=2 \
+        -net nic,model=virtio -net user,hostfwd=tcp::2222-:22 \
+        -boot d -cdrom "${iso}" -nographic -serial mon:stdio
+}
+
+run_qcow2() {
+    local -r img="${1}"
+    qemu-system-x86_64 -accel kvm -m 4096M -smp cores=2 \
+        -net nic,model=virtio -net user,hostfwd=tcp::2222-:22 \
+        -fw_cfg name=opt/com.coreos/config,file=metadata/key.ign \
+        -drive file="${img}"
+        # -nographic -serial mon:stdio -append 'console=ttyS0'
+}
+
++podman_run_cwd() {
+	local cmd=""
+	if [[ ${#} -eq 1 ]]; then
+		cmd="bash"
+	fi
+	podman run --rm --tty --interactive \
+		--security-opt label=disable \
+		--volume "${PWD}:/mnt" \
+		--tmpfs '/tmp:exec' \
+		--tmpfs '/var/tmp:exec' \
+		--workdir '/mnt' \
+		"${@}" \
+		bash
 }
