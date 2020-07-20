c3baeddd8ab65c2f718fbfc9b94428d433ccd7cf
Timothée Ravier <tim@siosm.fr>
2020-07-20 13:56:47 +0200

    update_all: support ostree & toolbox

--- update_all.sh
+++ update_all.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-if [[ -z "${TMUX+x}" ]]; then
-    tmux attach-session -t update || tmux new-session -s update -- ~/.local/bin/+update
-else
-    sudo dnf update --refresh
-    flatpak --system update --assumeyes
-    rustup update
-    cargo install-update -a
-    cargo cache -a
-    exec "${SHELL}"
+if [[ ! -f "/run/ostree-booted" ]]; then
+	if [[ -z "${TMUX+x}" ]]; then
+		tmux attach-session -t update || tmux new-session -s update -- ~/.local/bin/+update
+		exit 0
+	fi
 fi
+
+if [[ ! -f "/run/ostree-booted" ]]; then
+	sudo dnf update --refresh
+fi
+
+toolbox run --container fedora-toolbox-32 sudo dnf distro-sync -y
+flatpak --system update --assumeyes
+rustup update
+cargo install-update -a
+cargo cache -a
+exec "${SHELL}"
